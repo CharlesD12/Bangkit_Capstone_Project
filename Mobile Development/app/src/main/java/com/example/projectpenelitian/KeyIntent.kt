ce9e77bbcaaf26802a47ca6dd6e4eb0274467e2e
NaufalHammam <naufalteam2003@gmail.com>
2024-06-17 19:05:08 +0700

    MD-App

--- Mobile Development/app/src/main/java/com/example/projectpenelitian/KeyIntent.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projectpenelitian
-
-object KeyIntent {
-    const val INFORMATION_KEY = "information"
-    const val URI_KEY = "uri"
-}
--- Mobile Development/app/src/main/java/com/example/projectpenelitian/data/pref/UserModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projectpenelitian.data.pref
-
-data class UserModel(
-    val userId: String,
-    val name: String,
-    val email: String,
-    val token: String,
-    val isLogin: Boolean = false
-)
